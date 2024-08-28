7e53ce3a297adbf71e81ed8e6f340bbb1efd5e00
Zaid Marouf <zaidmarouf15@gmail.com>
2024-08-29 03:21:33 +0530

    Second Commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div className="relative">
+      <Header />
+      <div className="bg-black w-full h-screen absolute z-0 opacity-50"></div>
+      <img
+        className="w-full h-screen object-cover absolute -z-[1]"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/1d29f9a4-1900-43dc-a420-99044f734ee2/cc3b7bcb-3f79-449e-a37c-26ffb20fce3c/IN-en-20240826-POP_SIGNUP_TWO_WEEKS-perspective_WEB_7a193436-88c7-4f66-a8f0-e191d3b26d13_small.jpg"
+        alt="Background Image Login Screen"
+      />
+    </div>
+  );
+};
+
+export default Login;
