1bb186c6c2ff3248a16a297af817e97917e66194
Moin Qureshi <qureshimoin63@gmail.com>
2024-09-04 19:11:27 +0530

    Added Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import Browse from './components/Browse';
 import Login from './components/Login';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import appStore from './utils/appStore';
+import Error from './components/Error';
 
 function App() {
   const approuter = createBrowserRouter([
     {
       path: '/',
       element: <Login />,
+      errorElement: <Error/>
     },
     {
       path: '/browse',
       element: <Browse />,
+      errorElement: <Error/>
     },
+    
+    
   ]);
 
   return (
