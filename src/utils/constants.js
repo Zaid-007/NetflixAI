cb759b069a4eed8872678fd6b8412a960034d691
Zaid Marouf <zaidmarouf15@gmail.com>
2024-09-05 23:46:05 +0530

    Setup TMDB API & Created movie store

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const BACKGROUND_IMAGE =
 
 export const USER_AVATAR =
   'https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e';
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer ' + import.meta.env.VITE_TMDB_KEY,
+  },
+};
